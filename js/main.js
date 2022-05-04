ae10aaaeee9365ade8724d636c515a0617e5f6c8
Gustavo Osorio <gustavo.cgov@gmail.com>
2022-05-04 11:54:44 -0500

    v 0.1
    version 0.1

---
+++ js/main.js
@@ -0,0 +1,77 @@
+var ultCarg;
+
+window.onload = function(){
+    if (!setMenu()) return;
+    //window.addEventListener("scroll", function () {scrollEnd()}, false);
+    document.addEventListener(
+        'scroll',
+        (event) => {
+            scrollEnd();
+        }, 
+        { passive: true }
+    );
+  ultCarg = "inicio";
+};
+
+function scrollEnd() {
+    if (document.documentElement.scrollHeight <= window.innerHeight + window.pageYOffset + 1) 
+    {
+        if (ultCarg === "inicio") //if(document.getElementById("inicio").classList.contains("active"))
+        {
+          scrollNext("aboutme");
+          return;
+        }
+        if (ultCarg === "aboutme") //if(document.getElementById("aboutme").classList.contains("active"))
+        {
+          scrollNext("expLaboral");
+          return;
+        }
+        if (ultCarg === "expLaboral") //if(document.getElementById("expLaboral").classList.contains("active"))
+        {
+          scrollNext("contacto");
+          return;
+        }
+    }
+}
+
+function scrollNext(pagina) {
+  ultCarg = pagina;
+  request=new XMLHttpRequest();
+  request.overrideMimeType('text/xml');
+  request.onload = function(){
+    var d = document.createElement("div");
+    d.innerHTML = request.responseText;
+    document.getElementById("content").appendChild(d);
+  };
+  request.open("GET","html/"+pagina+".html");
+  request.send(null);
+}
+
+function setMenu() {
+    document.getElementById("inicio").addEventListener("click", function(){pageOpenFuncion("inicio")});
+    document.getElementById("inicio-sm").addEventListener("click", function(){pageOpenFuncion("inicio")});
+    document.getElementById("aboutme").addEventListener("click", function(){pageOpenFuncion("aboutme")});
+    document.getElementById("aboutme-sm").addEventListener("click", function(){pageOpenFuncion("aboutme")});
+    document.getElementById("expLaboral").addEventListener("click", function(){pageOpenFuncion("expLaboral")});
+    document.getElementById("expLaboral-sm").addEventListener("click", function(){pageOpenFuncion("expLaboral")});
+    document.getElementById("contacto").addEventListener("click", function(){pageOpenFuncion("contacto")});
+    document.getElementById("contacto-sm").addEventListener("click", function(){pageOpenFuncion("contacto")});
+    pageOpenFuncion("inicio");
+    return true;
+}
+
+function pageOpenFuncion(arg) {
+  ultCarg = arg;
+  request=new XMLHttpRequest();
+  request.overrideMimeType('text/xml');
+
+  request.onload = function(){
+    document.getElementById("content").innerHTML = request.responseText;
+  };
+  
+  request.open("GET","html/"+arg+".html",false);
+  request.send(null); 
+}
+
+
+
