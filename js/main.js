017beb7398a4fd586b606a436f903432ca316eab
Gustavo Osorio <gustavo.cgov@gmail.com>
2022-05-06 13:32:02 -0500

    v 0.2
    Actualizacion de estilos y metadatos

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ window.onload = function(){
         }, 
         { passive: true }
     );
+    window.addEventListener("scroll", reveal);
   ultCarg = "inicio";
 };
 
@@ -73,5 +74,21 @@ function pageOpenFuncion(arg) {
   request.send(null); 
 }
 
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
 
 
